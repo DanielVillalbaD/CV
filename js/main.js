7c6839ddfc030113884e0a0850d0e6f7cc43b417
Daniel Villalba <villalbadur@gmail.com>
2017-12-15 02:47:28 +0100

    index JS and style.css minified

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ $(".toggle-icon").click(function() {
   $(".overlay").toggleClass("anim");
 });
 
-$(".animation").click(function() {
-  $(".anim").toggleClass("reverse-animation");
-});
 $(".toggle-icon").click(function() {
   $("#nav-container").toggleClass("pushed");
 });
