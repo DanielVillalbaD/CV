982cf403bbf733fb9bd5e516426ac4df38b20376
Daniel Villalba <villalbadur@gmail.com>
2018-01-04 23:15:41 +0100

    Slider education finish

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* SABER LASER */
+
 $(".toggle-icon").click(function() {
   $(".overlay").toggleClass("anim");
 });
@@ -5,3 +7,23 @@ $(".toggle-icon").click(function() {
 $(".toggle-icon").click(function() {
   $("#nav-container").toggleClass("pushed");
 });
+
+/* EDUCATION & EXPERIENCE TABS */
+
+$(".startabs").tabs({ 
+  show: ('slow', { duration: 800 }) ,
+  hide: ('slow', { duration: 400 }) 
+});
+
+$("#educacion").tabs({ 
+  show: ('slow', { duration: 600 }) ,
+  hide: ('slow', { duration: 300 }) 
+});
+
+/* VIDEO PLAYER */
+
+//GET ELEMENTS
+
+//FUNCTIONS
+
+//EVENT LISTENERS
